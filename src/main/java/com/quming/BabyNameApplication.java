c405d399ff8cc3a0776f7769bb8fac51e8847c0f
xzbd <xzbd745114565@163.com>
2019-09-16 21:04:02 +0800

    Initial commit

---
+++ src/main/java/com/quming/BabyNameApplication.java
@@ -0,0 +1,13 @@
+package com.quming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyNameApplication.class, args);
+    }
+
+}
